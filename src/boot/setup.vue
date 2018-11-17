626bfd4e75f5f09381ba181c7fda34d9e5a86c31
armin <arminkheirkhahan@gmail.com>
2018-11-17 17:55:35 +0330

    work verion

--- src/boot/setup.vue
+++ src/boot/setup.vue
@@ -1,24 +1,23 @@
 <template>
   <view class="container">
-    <app-loading v-if="!isAppReady"> </app-loading>
+    <!-- <app-loading v-if="!isAppReady"> </app-loading> -->
     <app v-if="isAppReady"></app>
   </view>
 </template>
 
 
 <script>
-import Vue from "vue-native-core";
-import { StackNavigator } from "vue-native-router";
-import { VueNativeBase } from "native-base";
-import { AppLoading } from "expo";
+import Vue from 'vue-native-core';
+import { StackNavigator } from 'vue-native-router';
+import { VueNativeBase } from 'native-base';
 
-import App from "../App.vue";
+import App from '../App.vue';
 
 // registering all native-base components to the global scope of the Vue
 Vue.use(VueNativeBase);
 
 export default {
-  components: { App, AppLoading },
+  components: { App },
   data: function() {
     return {
       isAppReady: false
@@ -31,14 +30,14 @@ export default {
     loadFonts: async function() {
       try {
         this.isAppReady = false;
-        await Expo.Font.loadAsync({
-          Roboto: require("native-base/Fonts/Roboto.ttf"),
-          Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-          Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
-        });
+        // await Expo.Font.loadAsync({
+        //   Roboto: require("native-base/Fonts/Roboto.ttf"),
+        //   Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+        //   Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+        // });
         this.isAppReady = true;
       } catch (error) {
-        console.log("some error occured", error);
+        console.log('some error occured', error);
         this.isAppReady = true;
       }
     }
